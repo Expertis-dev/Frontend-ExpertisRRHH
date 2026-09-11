8c099b3e59df4634f8ea8e4dfd1819a78d2ae5d2
Sebastian Guzman <sebastian.guzman@goexpertis.com>
2026-09-11 11:17:27 -0500

    mostrar detalle de procedencia

--- src/view/Empleado/ListarEmpleados.jsx
+++ src/view/Empleado/ListarEmpleados.jsx
@@ -2030,12 +2030,6 @@ export const ListarEmpleados = () => {
                         <span className="font-semibold">Num.Hijos:</span>{" "}
                         {selectedEmployee.nroHijos}
                       </p>
-                      <p>
-                        <span className="font-semibold">Procedencia:</span>{" "}
-                        {selectedEmployee.procedencia
-                          ? selectedEmployee.procedencia
-                          : "N/A"}
-                      </p>
                     </div>
                     <div className="space-y-1">
                       <p>
@@ -2061,6 +2055,18 @@ export const ListarEmpleados = () => {
                       <p>
                         <span className="font-semibold">Cant. Ingresos:</span>{" "}
                         {selectedEmployee.CantidadIngresos}
+                      </p>
+                      <p>
+                        <span className="font-semibold">Procedencia:</span>{" "}
+                        {selectedEmployee.procedencia
+                          ? selectedEmployee.procedencia
+                          : "N/A"}
+                      </p>
+                      <p>
+                        <span className="font-semibold">Detalle procedencia:</span>{" "}
+                        {selectedEmployee.detProcedencia
+                          ? selectedEmployee.detProcedencia
+                          : "N/A"}
                       </p>
                     </div>
                     <div className="space-y-1">
